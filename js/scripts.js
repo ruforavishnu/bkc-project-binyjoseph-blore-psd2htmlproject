dd8a97d8dbdf0427849015382d3f4d581d13d67a
Vishnu Ajit <vishnu@ruforaweb.com>
2018-07-15 21:57:30 +0530

    added 5th testimonial as per given by biny and middle aligned the text contactus on the menu item of contactus

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,15 @@
 	{
 
 		
-		var $total_testimonials = 4;
+		var $total_testimonials = 5;
 		var $index = 0;
 		var $testimonials_content = 
 		 [
 		 	"M/s Baikunje Consultancy, a Bangalore-based firm, evaluated the parameters required for implementing the Good Distribution Practices for Ibn Sina Pharmacy LLC, Muscat, Sultanate of Oman. The professional services offered by Baikunje Consultancy enabled the GDP system implementation at our end. Their ‘hands-on efforts’ are appreciated. We wish them the very best!!",
 		 	"At Sovereign Pharma Pvt Ltd (SRPL), quality system, data integrity and reliability, policy, procedure developmentand implementation were supported by the Baikunje Consultancy.We also utilised their service for SAP system validation compliance in accordance with GAMP-5. This has helped us upgrade our pharmaceutical quality system in compliance with regulatory expectations. We are very happy with their services, which we have used multiple times. SPPL Team thanks them for continued support in a highly professional manner.With Regards," ,
 		 	"For our clinical study in India, Umesh (Baikunje Consultancy) monitored it for GCP compliance and submitted a well-written report. We are very pleased with his professionalism and the alacrity with which he did the work. Thanks, Umesh. It was great to be associated with you.",
-		 	"I had an opportunity to work with Umesh Baikunje for one of the Stemade projects wherein he consulted us on GMP requirement and compliance for dental stem cell banking. His technical expertise and vast experience in this field was of immense help in this project. Wish him all the best in this endeavour and would surely look forward to working with him in the future too!!!"
+		 	"I had an opportunity to work with Umesh Baikunje for one of the Stemade projects wherein he consulted us on GMP requirement and compliance for dental stem cell banking. His technical expertise and vast experience in this field was of immense help in this project. Wish him all the best in this endeavour and would surely look forward to working with him in the future too!!!",
+		 	"We collaborated with Baikunje Consultancy to design and deliver training workshops for GxP training requirements. Mr. Umesh Baikunje’s vast experience and practical examples brought the training session to life. We look forward to work in future projects."
 		 ];
 
 		 var $testimonials_footer =
@@ -22,7 +23,8 @@
 		 	"P.S.Ramesh Prakash, General Manager, Ibn Sina Pharmacy LLC, Sultanate of Oman",
 		 	"Mr. Piyush V. Desai and team, Director-Operations, M/sSovereign Pharma Pvt. Ltd., Nani Daman, India",
 		 	"Dr. Salim Shah, President Sarfez Pharmaceuticals, Vienna, USA",
-		 	"Rajashri Mokashi, Chief Operating Officer, Stemade Biotech Pvt. Ltd, Mumbai"
+		 	"Rajashri Mokashi, Chief Operating Officer, Stemade Biotech Pvt. Ltd, Mumbai",
+		 	"BiOZEEN"
 		 ];
 
 		 //MS Soverigne Pharma pvt ltd. in one lin
@@ -31,7 +33,7 @@
 		 {
 		 	$index+=1
 
-			if($index == 4)
+			if($index == 5)
 			{
 				$index = 0;
 			}	
